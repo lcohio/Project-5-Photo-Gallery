b439a96c17ad42c454a07f1aca31c09cdbcdab32
lcohio <coninglogan@gmail.com>
2019-01-17 18:30:18 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    // Variables
+    var input, filter, wrapper, a, i, txtValue;
+    input = document.getElementById('site-search');
+    filter = input.value.toUpperCase();
+    wrapper = document.getElementsByClassName("gallery-wrapper");
+    a = getElementsByTagName('a');
+
+
+    for (i = 0; i < a.length; i++) {
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          a[i].style.display = "";
+        } else {
+          a[i].style.display = "none";
+        }
+      }
+}
