3f65f28bc8316e2c5592d5003163eecd3f942e32
lcohio <coninglogan@gmail.com>
2019-01-17 22:23:59 -0500

    Pending review

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
 function myFunction() {
-  // Variables
-  var input, filter, wrapper, a, i, txtValue;
-  input = document.getElementById('site-search');
-  filter = input.value.toUpperCase();
-  wrapper = document.getElementsByClassName("gallery-wrapper");
-  a = getElementsByTagName('a');
+// Variables
+var input, filter, wrapper, a, i, txtValue;
+input = document.getElementById('site-search');
+filter = input.value.toUpperCase();
+wrapper = document.getElementsByClassName("gallery-wrapper");
+a = document.querySelectorAll('.gallery-wrapper a');
 
-
-  for (i = 0; i < a.length; i++) {
-      let txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
+// Loop through each title attribute on search and hide unsearched content
+for (i = 0; i < a.length; i++) {
+    let txtValue = a[i].getAttribute('title');
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
       } else {
         a[i].style.display = "none";
       }
     }
 }
+
+// Lightbox Settings
+
+lightbox.option({
+  'resizeDuration': 400,
+  'wrapAround': true,
+  'albumLabel': ""
+});
