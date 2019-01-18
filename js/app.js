660e48e927345f6823fdf82315b1215a4b657211
lcohio <coninglogan@gmail.com>
2019-01-17 20:52:49 -0500

    Working on search

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 function myFunction() {
-    // Variables
-    var input, filter, wrapper, a, i, txtValue;
-    input = document.getElementById('site-search');
-    filter = input.value.toUpperCase();
-    wrapper = document.getElementsByClassName("gallery-wrapper");
-    a = getElementsByTagName('a');
+  // Variables
+  var input, filter, gallery, a, i, txtValue;
+  input = document.getElementById('site-search);
+  filter = input.value.toUpperCase();
+  gallery = document.getElementsByClassName("gallery-wrapper");
+  a = a.getElementsByTagName('a');
 
-
-    for (i = 0; i < a.length; i++) {
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          a[i].style.display = "";
-        } else {
-          a[i].style.display = "none";
-        }
-      }
+  for (i = 0; i < li.length; i++) {
+    let a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
 }
